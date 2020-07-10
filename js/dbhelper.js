e0228b3854f02e77117dcd279162d1c1a2c1a90d
Whishes <nicholas.cruickshank25@gmail.com>
2020-07-10 20:36:45 +1000

    Add files via upload
    Resubmission with fixes

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
@@ -168,4 +168,3 @@ class DBHelper {
   }
 
 }
-
